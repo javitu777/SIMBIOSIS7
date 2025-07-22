38ef38a0a731691fceed1fbe6f47cf38fa258473
javitu777 <javiturbe787@gmail.com>
2025-07-22 12:49:14 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
  import React from "react";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <h1>AdGame Platform</h1>
       <p>Bienvenido a la plataforma de anuncios para videojuegos</p>
+      <Login />
     </div>
   );
 }
